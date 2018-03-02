35c7aafb24223121f79d3d2f193afbc7d768cdb9
wuqiong6 <wuqiong6@tuniu.com>
2018-03-02 17:37:08 +0800

    Spring4.x 数据库连接信息，源码，sql

---
+++ chapter2/src/main/resources/Spring4.x/sampledb.sql
@@ -0,0 +1,54 @@
+/*
+SQLyog Ultimate v12.08 (64 bit)
+MySQL - 5.7.20-log : Database - sampledb
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`sampledb` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `sampledb`;
+
+/*Table structure for table `t_login_log` */
+
+DROP TABLE IF EXISTS `t_login_log`;
+
+CREATE TABLE `t_login_log` (
+  `login_log_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `ip` varchar(23) DEFAULT NULL,
+  `login_datetime` datetime DEFAULT NULL,
+  PRIMARY KEY (`login_log_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `t_login_log` */
+
+/*Table structure for table `t_user` */
+
+DROP TABLE IF EXISTS `t_user`;
+
+CREATE TABLE `t_user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(30) DEFAULT NULL,
+  `credits` int(11) DEFAULT NULL,
+  `password` varchar(32) DEFAULT NULL,
+  `last_visit` datetime DEFAULT NULL,
+  `last_ip` varchar(23) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Data for the table `t_user` */
+
+insert  into `t_user`(`user_id`,`user_name`,`credits`,`password`,`last_visit`,`last_ip`) values (1,'admin',NULL,'123456',NULL,NULL);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
